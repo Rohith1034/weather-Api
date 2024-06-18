7e12d5086d26ccdad7408a3cdf9a4cfafe447355
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2024-06-18 21:22:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+echo "# weather-Api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Rohith1034/weather-Api.git
+git push -u origin main
+
+git remote add origin https://github.com/Rohith1034/weather-Api.git
+git branch -M main
+git push -u origin main
---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+const path = require("path"); // Correctly import the path module
+
+const app = express();
+let weatherData;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
+
+app.get("/", async(req, res) => {
+    const response = await axios.get("https://api.weatherapi.com/v1/forecast.json?key=7931b0d8a0fe4328afc151038241406&q=dharmavaram", { timeout: 5000 });
+        weatherData = response.data;
+        res.status(200).json(weatherData);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server started on port "+ process.env.PORT);
+});
